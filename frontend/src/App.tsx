d8a4e70b02d84e2d55809b54833fe9cc2815dfbc
Vitor Lucio <lucivitor@gmail.com>
2021-11-06 02:03:21 -0300

    Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import NavBar from "components/NavBar";
+
 function App() {
   return (
     <div>
       <h1 className="text-primary">Olá Mundo!</h1>
+      <NavBar />
+      <NavBar />
+      <NavBar />
+      <NavBar />
     </div>
   );
 }
